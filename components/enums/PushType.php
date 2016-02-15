e064bd6b39abed74a39952ac5ae598d3bc9e0131
Alexander Lushnikov <alegz@therealstart.com>
2016-02-15 14:02:00 +0500

    Added PushType enum with list of valid types for PushDevice type. Added validation for field type in PushDevice model.

---
+++ components/enums/PushType.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Date: 2/15/16
+ * Time: 2:00 PM
+ */
+
+namespace TRS\AsyncNotification\components\enums;
+
+
+class PushType
+{
+    const ANDROID = 'android';
+    const IOS = 'ios';
+}
--- models/PushDevice.php
+++ models/PushDevice.php
@@ -2,12 +2,21 @@
 
 namespace TRS\AsyncNotification\models;
 
+use TRS\AsyncNotification\components\enums\PushType;
 use Yii;
 use TRS\AsyncNotification\models\base\PushDevice as BasePushDevice;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "push_device".
  */
 class PushDevice extends BasePushDevice
 {
+    public function rules()
+    {
+        return ArrayHelper::merge(parent::rules(), [
+            ['type', 'in', 'range' => [PushType::ANDROID, PushType::IOS]]
+        ]);
+    }
+
 }
