8def4cf577589e22bc791c57dffd2ef860b9ced0
Alexander Lushnikov <alegz@therealstart.com>
2016-02-15 11:57:32 +0500

    Added table push_device with migration. Added model for PushDevice model for table.

---
+++ migrations/m160212_132037_add_phone_id_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160212_132037_add_phone_id_table extends Migration
+{
+    const PUSH_DEVICE = 'push_device';
+    public function safeUp()
+    {
+        $this->createTable(static::PUSH_DEVICE, [
+            'id' => Schema::TYPE_PK,
+            'type' => Schema::TYPE_STRING . ' NOT NULL',
+            'device_id' => Schema::TYPE_STRING . '(128) NOT NULL',
+            'owner_id' => Schema::TYPE_STRING,
+            'created_at' => Schema::TYPE_TIMESTAMP . ' NOT NULL'
+        ]);
+
+        $this->createIndex(static::PUSH_DEVICE . '_device_id', static::PUSH_DEVICE, 'device_id');
+        $this->createIndex(static::PUSH_DEVICE . '_owner_id', static::PUSH_DEVICE, 'owner_id');
+    }
+
+    public function safeDown()
+    {
+        $this->dropIndex(static::PUSH_DEVICE . '_owner_id', static::PUSH_DEVICE);
+        $this->dropIndex(static::PUSH_DEVICE . '_device_id', static::PUSH_DEVICE);
+
+        $this->dropTable('notification_device');
+    }
+}
---
+++ models/PushDevice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TRS\AsyncNotification\models;
+
+use Yii;
+use TRS\AsyncNotification\models\base\PushDevice as BasePushDevice;
+
+/**
+ * This is the model class for table "push_device".
+ */
+class PushDevice extends BasePushDevice
+{
+}
