2a3839317d63f014528a1fccb3204eb4403dd40f
grr89 <grr89@mail.ru>
2016-07-19 11:13:24 +0500

    DeviceOS enum added

---
+++ components/enums/DeviceOS.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Date: 2/15/16
+ * Time: 2:00 PM
+ */
+
+namespace TRS\AsyncNotification\components\enums;
+
+
+class DeviceOS
+{
+    const ANDROID = 'android';
+    const IOS = 'ios';
+}
